5e753e9fb5dc97f776b7fd3e21daf53bc6cf92ea
Casey Seagle <caseyseagle8913@gmail.com>
2026-03-31 15:07:41 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[7]={4,8,3,6,2,5,7};
+    int temp;
+
+
+    for(int i=0;i<7;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    for(int x=0;x<7;x++)
+    {
+        for(int y=0;y<7;y++)
+        {
+            if(arr[y]>arr[y+1])
+            {
+                temp=arr[y];
+                arr[y]=arr[y+1];
+                arr[y+1]=temp;
+            }
+        }
+    }
+    cout << endl;
+     for(int i=0;i<7;i++)
+        {
+            cout << arr[i] << " ";
+        }
+}
+
